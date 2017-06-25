4fbcb37ad66f3ae24e15f89dd38737bbb0745aff
juju4 <juju4@users.noreply.github.com>
2017-06-25 12:32:12 -0400

    Initial commit to github

---
+++ test/integration/default/serverspec/chromeconfig_spec.rb
@@ -0,0 +1,23 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+## Use Junit formatter output, supported by jenkins
+#require 'yarjuf'
+#RSpec.configure do |c|
+#    c.formatter = 'JUnit'
+#end
+
+describe file('/usr/share/google-chrome/extensions/cjpalhdlnbpafiamejdnhcphjbkeiagm.json') do
+  it { should be_file }
+  it { should be_mode 0644 }
+end
+describe file('/usr/share/google-chrome/extensions/gcbommkclmclpchllfjekcdonpmejbdp.json') do
+  it { should be_file }
+  it { should be_mode 0644 }
+end
+describe file('/usr/share/google-chrome/extensions/pkehgijcmpdhfbdbbnkijodmdjhbjlgp.json') do
+  it { should be_file }
+  it { should be_mode 0644 }
+end
